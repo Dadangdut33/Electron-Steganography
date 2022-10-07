c03bc7456fec0183149b97f3ea32de0ef49a7eda
Ammar Sufyan <63483228+neouranos@users.noreply.github.com>
2022-10-08 01:01:37 +0700

    Add fa

--- src/index.ts
+++ src/index.ts
@@ -7,88 +7,88 @@ let mainWindow: BrowserWindow | null = null;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
-	// eslint-disable-line global-require
-	app.quit();
+  // eslint-disable-line global-require
+  app.quit();
 }
 
 const createWindow = (): void => {
-	// Create the browser window.
-	mainWindow = new BrowserWindow({
-		width: 800,
-		height: 600,
-		minWidth: 800,
-		minHeight: 600,
-		webPreferences: {
-			preload: path.join(__dirname, "./preload.js"),
-			nodeIntegration: true,
-			contextIsolation: false,
-		},
-	});
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    minWidth: 800,
+    minHeight: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "./preload.js"),
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
 
-	// and load the index.html of the app.
-	mainWindow.loadFile(path.join(__dirname, "../src/index.html"));
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, "../src/index.html"));
 
-	// if unreponsive, show dialog box
-	mainWindow.on("unresponsive", onUnresponsiveWindow);
+  // if unreponsive, show dialog box
+  mainWindow.on("unresponsive", onUnresponsiveWindow);
 
-	// prevent new window from opening
-	mainWindow.webContents.on("new-window", function (event, url) {
-		event.preventDefault();
-	});
+  // prevent new window from opening
+  mainWindow.webContents.on("new-window", function (event, url) {
+    event.preventDefault();
+  });
 
-	// Open the DevTools.
-	mainWindow.webContents.openDevTools();
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on("ready", () => {
-	createWindow();
+  createWindow();
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
-	if (process.platform !== "darwin") {
-		app.quit();
-	}
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
 app.on("activate", () => {
-	// On OS X it's common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
-	if (BrowserWindow.getAllWindows().length === 0) {
-		createWindow();
-	}
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 });
 
 // --------------------------------
 // menu
 const menu = new Menu();
 menu.append(
-	new MenuItem({
-		label: "Quit",
-		click: () => {
-			app.exit(0);
-		},
-	})
+  new MenuItem({
+    label: "Quit",
+    click: () => {
+      app.exit(0);
+    },
+  })
 );
 
-Menu.setApplicationMenu(menu);
+//Menu.setApplicationMenu(menu);
 
 /**
  * Run when the app is unresponsive
  */
 function onUnresponsiveWindow() {
-	const window = BrowserWindow.getFocusedWindow();
-	dialog.showMessageBox(window, {
-		title: "Application is not responding",
-		buttons: ["Dismiss"],
-		type: "warning",
-		message: "Application is not responding…",
-	});
+  const window = BrowserWindow.getFocusedWindow();
+  dialog.showMessageBox(window, {
+    title: "Application is not responding",
+    buttons: ["Dismiss"],
+    type: "warning",
+    message: "Application is not responding…",
+  });
 }
 
 // In this file you can include the rest of your app's specific main process
